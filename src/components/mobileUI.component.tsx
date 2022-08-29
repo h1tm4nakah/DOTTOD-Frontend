b8b5cd3fffe072819c88a7b158b545486c8c4334
Pietro Rustici <p.rustici93@gmail.com>
2022-08-29 09:34:07 +0200

    updated UI

--- src/components/mobileUI.component.tsx
+++ src/components/mobileUI.component.tsx
@@ -14,7 +14,7 @@ export function MobileUI({flip, setFlip, focusIDX, handleIDXChange}: MobileUIPro
     return (
         <div className="mobile-ui">
             <div className="d-flex justify-content-between align-items-center h-100">
-                <div onClick={() => handleIDXChange(-1)} className="px-4 py-3" style={{borderRight: "2px solid black"}}>
+                <div onClick={() => handleIDXChange(-1)} className="px-4 py-2" style={{borderRight: "2px solid black"}}>
                     <b>←</b>
                 </div>
                 <div className="">
@@ -22,7 +22,7 @@ export function MobileUI({flip, setFlip, focusIDX, handleIDXChange}: MobileUIPro
                         { flip ? t('UI_MENU_HIDE') : t('UI_MENU_SHOW') }
                     </b>
                 </div>
-                <div onClick={() => handleIDXChange(1)} className="px-4 py-3" style={{borderLeft: "2px solid black"}}>
+                <div onClick={() => handleIDXChange(1)} className="px-4 py-2" style={{borderLeft: "2px solid black"}}>
                     <b>→</b>
                 </div>
             </div>
